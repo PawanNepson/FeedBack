c4fa3753608eb2fe37102bb3dfe8d27105f30833
Pawan <nepsonh@gmail.com>
2022-03-15 15:01:16 +0545

    hhh

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,18 +1,18 @@
 export const FeedbackData = [
     {
         id: 1,
-        rating: 9,
-        text: 'This movie, at a glance, seemed somewhat appealing. However, 10 minutes into the movie... I began to realize other wise. The plot was bland and wanna-be progressive while the characters were all unlikeable',
+        rating: 6,
+        text: 'This movie, at a glance, seemed somewhat appealing. However, 10 minutes into the movie... I began to realize other wise. The plot was bland and wanna-be progressive while the characters were all unlikeable 😒😒😒',
     },
     {
         id: 2,
-        rating: 4,
-        text: 'Some Freaks is definitely not a "feel-good" movie',
+        rating: 9,
+        text: 'Is definitely not a "feel-good" movie🍿🎥📽️',
     },
     {
         id: 3,
-        rating: 10,
-        text: 'movie started with potential but quickly gets lost.',
+        rating: 4,
+        text: 'movie started with potential but quickly gets lost 💔💔💔.',
     },
 ];
 
