40bfa33ac1dcb7a78181895abfb9b474893f9367
Pawan <nepsonh@gmail.com>
2022-03-18 14:32:51 +0545

    Relevent icon Fatimescircle imported from react icon for deleting review

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from './shared/Card';
+import { FaTimesCircle } from 'react-icons/fa';
 
 function FeedbackItem({ item, handleDelete }) {
     return (
         <Card>
             <div className='num-display'>{item.rating}</div>
             <div className='text-display'>{item.text}</div>
-            <button onClick={() => handleDelete(item.id)}>Delete</button>
+            <FaTimesCircle onClick={() => handleDelete(item.id)} className='close ' color='purple' size='2rem' />
+
+
         </Card>
     );
 }
