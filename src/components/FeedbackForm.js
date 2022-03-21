d8a22ea58d80251c2c6a8a2d32dee828be9a4f13
Pawan <nepsonh@gmail.com>
2022-03-21 15:37:41 +0545

    Feedback form and reusable Button Component added

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from './shared/Button';
+import Card from './shared/Card';
+
+function FeedbackForm() {
+    return (
+        <Card>
+            <form>
+                <h2>Please share your review .</h2>
+                <div className='input-group'>
+                    <input type='text' />
+                    <Button> Submit </Button>
+                </div>
+            </form>
+        </Card>
+    );
+}
+
+export default FeedbackForm;
