296aa2000c0f3914fd09182e9c320a9ce09adc3a
Pawan <nepsonh@gmail.com>
2022-03-25 12:31:36 +0545

    validation done ratingatar and rendered on ui

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,27 +1,27 @@
-// import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 export const FeedbackData = [
     {
-        id: 1,
+        id: uuidv4(),
         rating: 6,
         text: 'This movie, at a glance, seemed somewhat appealing. However, 10 minutes into the movie... I began to realize other wise. The plot was bland and wanna-be progressive while the characters were all unlikeable 😒😒😒',
     },
     {
-        id: 2,
+        id: uuidv4(),
         rating: 9,
         text: 'Is definitely  a "feel-good" movie🍿🎥📽️',
     },
     {
-        id: 3,
+        id: uuidv4(),
         rating: 4,
         text: 'movie started with potential but quickly gets lost 💔💔💔.',
     },
     {
-        id: 4,
+        id: uuidv4(),
         rating: 7,
         text: 'This movie is best in recent days.⚒️⚒️⚒️',
     },
     {
-        id: 5,
+        id: uuidv4(),
         rating: 10,
         text: 'This is the movie you have been waiting for 👍🏻👍🏻👍🏻💯💯💯',
     },
